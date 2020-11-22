3953b77503df2e43923f663e1660fa7c409c09e7
Alexander <kucherenko.av@phystech.edu>
2020-11-22 21:23:05 +0300

    Refactored according to SOLID principles

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,37 +1,31 @@
 package ru.sbt.mipt.oop;
 
-import com.google.gson.Gson;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import static ru.sbt.mipt.oop.SensorEventType.*;
-
 public class Application {
+    private EventGenerator eventGenerator;
+    private SmartHomeLoader smartHomeLoader;
+    private EventHandler eventHandler;
 
-    public static void main(String... args) throws IOException {
-        // считываем состояние дома из файла
-        Gson gson = new Gson();
-        String json = new String(Files.readAllBytes(Paths.get("smart-home-1.js")));
-        SmartHome smartHome = gson.fromJson(json, SmartHome.class);
-        // начинаем цикл обработки событий
-        SensorEvent event = getNextSensorEvent();
+    public Application(EventGenerator eventGenerator, SmartHomeLoader smartHomeLoader) {
+        this.eventGenerator = eventGenerator;
+        this.smartHomeLoader = smartHomeLoader;
+    }
+
+    public static void main(String... args) {
+        new Application(
+                new RandomEventGenerator(),
+                new SmartHomeJsonLoader("smart-home-1.json")
+        ).run();
+    }
+
+    private void run() {
+        SensorEvent event = eventGenerator.getNextEvent();
+        SmartHome smartHome = smartHomeLoader.load();
+        eventHandler = new EventHandler(smartHome);
+
         while (event != null) {
             System.out.println("Got event: " + event);
-            event = getNextSensorEvent();
+            eventHandler.handle(event);
+            event = eventGenerator.getNextEvent();
         }
-    }
-
-    private static void sendCommand(SensorCommand command) {
-        System.out.println("Pretent we're sending command " + command);
-    }
-
-    private static SensorEvent getNextSensorEvent() {
-        // pretend like we're getting the events from physical world, but here we're going to just generate some random events
-        if (Math.random() < 0.05) return null; // null means end of event stream
-        SensorEventType sensorEventType = SensorEventType.values()[(int) (4 * Math.random())];
-        String objectId = "" + ((int) (10 * Math.random()));
-        return new SensorEvent(sensorEventType, objectId);
     }
 }
--- src/main/java/ru/sbt/mipt/oop/SmartHome.java
+++ src/main/java/ru/sbt/mipt/oop/SmartHome.java
@@ -3,8 +3,6 @@ package ru.sbt.mipt.oop;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import static ru.sbt.mipt.oop.SensorEventType.*;
-
 public class SmartHome {
     private Collection<Room> rooms;
 
@@ -22,14 +20,5 @@ public class SmartHome {
 
     public Collection<Room> getRooms() {
         return rooms;
-    }
-
-    public void handleEvent(SensorEvent event){
-        if (event.getType() == LIGHT_ON || event.getType() == LIGHT_OFF) {
-            // событие от источника света
-            for (Room room : rooms) {
-                room.handleEvent(event);
-            }
-        }
     }
 }
